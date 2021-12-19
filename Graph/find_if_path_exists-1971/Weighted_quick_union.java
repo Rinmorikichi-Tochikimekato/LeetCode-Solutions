c01a804e11c281d568ac7d3799bf28b4f4ab252f
Mohit Nirwan <40948557+Rinmorikichi-Tochikimekato@users.noreply.github.com>
2021-12-19 21:42:02 +0530

    Create Weighted_quick_union.java
    Runtime: 13 ms, faster than 95.47% of Java online submissions for Find if Path Exists in Graph.
    Memory Usage: 95 MB, less than 92.66% of Java online submissions for Find if Path Exists in Graph.

---
+++ Graph/find_if_path_exists-1971/Weighted_quick_union.java
@@ -0,0 +1,67 @@
+class Solution {
+     //union find Simple, faster than initial
+    int[] uf;
+    int[] sz;
+    public boolean validPath(int n, int[][] edges, int start, int end) {
+        
+    uf = new int[n];
+    sz = new int[n];   
+    
+    Arrays.fill(sz,1);
+        
+    QuickUF(uf);
+  
+    for(int[] edge:edges){
+        quickUnion(edge[0],edge[1]);
+    }
+    
+    // print(uf);
+    return isConnected(start,end);
+}
+    
+    
+    
+    //Print UF
+    void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+    
+    //function to init union find
+    void QuickUF(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            arr[i] = i;
+        }
+    }
+    
+    //finds parent recursively
+    int findParent(int i){
+        while(i!=uf[i]){
+            i = uf[i];
+        }
+        return i;
+    }
+    
+    //function to unionize the graph
+    void quickUnion(int p, int q){
+        int pParent = findParent(p);
+        int qParent = findParent(q);
+        
+        if(sz[p] < sz[q]){
+            sz[q]+=sz[p];
+            uf[pParent] = qParent;
+        }else{
+             sz[p]+=sz[q];
+              uf[qParent] = pParent;
+        }
+        
+     
+    }
+    
+    //function to check if they are connected
+    boolean isConnected(int p, int q){
+        return findParent(p) == findParent(q);
+    }
+}   
