c67a6762345c70dd792e6075a802c8f75ad00157
Mohit Nirwan <40948557+Rinmorikichi-Tochikimekato@users.noreply.github.com>
2021-12-19 19:18:32 +0530

    Create Quick_Union.java
    Runtime: 468 ms, faster than 5.03% of Java online submissions for Find if Path Exists in Graph.
    Memory Usage: 118 MB, less than 81.40% of Java online submissions for Find if Path Exists in Graph.

---
+++ Graph/find_if_path_exists-1971/Quick_Union.java
@@ -0,0 +1,56 @@
+class Quick_Union {
+     //union find Simple
+    int[] uf;
+    
+    public boolean validPath(int n, int[][] edges, int start, int end) {
+        
+    uf = new int[n];
+        
+    QuickUF(uf);
+  
+    for(int[] edge:edges){
+        quickUnion(edge[0],edge[1]);
+    }
+    
+    print(uf);
+    return isConnected(start,end);
+}
+    
+    
+    
+    //Print UF
+    void print(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+    
+    //function to init union find
+    void QuickUF(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            arr[i] = i;
+        }
+    }
+    
+    //finds parent recursively
+    int findParent(int i){
+        while(i!=uf[i]){
+            i = uf[i];
+        }
+        return i;
+    }
+    
+    //function to unionize the graph
+    void quickUnion(int p, int q){
+        int pParent = findParent(p);
+        int qParent = findParent(q);
+        
+       uf[qParent] = pParent;
+    }
+    
+    //function to check if they are connected
+    boolean isConnected(int p, int q){
+        return findParent(p) == findParent(q);
+    }
+}   
