09013c824315e3947174bb4252ef4c6786d07a76
Mohit Nirwan <40948557+Rinmorikichi-Tochikimekato@users.noreply.github.com>
2022-05-09 08:43:56 +0530

    Create IntegerPalindrome.java
    integer palindrome

---
+++ IntegerPalindrome.java
@@ -0,0 +1,58 @@
+// 9. Palindrome Number
+// Easy
+
+// 5747
+
+// 2120
+
+// Add to List
+
+// Share
+// Given an integer x, return true if x is palindrome integer.
+
+// An integer is a palindrome when it reads the same backward as forward.
+
+// For example, 121 is a palindrome while 123 is not.
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if(x<0 || (x!=0 && x%10 == 0)) return false;
+        
+        int revertNumber  = 0;
+        while(x>revertNumber){
+            revertNumber = revertNumber*10 + x%10;
+            x = x/10;
+        }
+        
+        if(x == revertNumber || x == revertNumber/10)
+        return true;
+        
+            return false;
+        
+    }
+}
+ 
